a63acee8bf6c3381948bd897b9acef2e7e4a2ec9
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2018-01-19 22:00:25 +0530

    Initial Commit
    Signed-off-by: Apoorvaa Gupta <apoorvaagupta01@gmail.com>

---
+++ js/index.js
@@ -0,0 +1,19 @@
+$(function () {
+
+  var zip = new JSZip();
+  zip.folder('test').file("test.txt", "hey there!!!!");
+  zip.generateAsync({type: "blob"}, function updateCallback(metadata) {
+    // var msg = "progression : " + metadata.percent.toFixed(2) + " %";
+    // if(metadata.currentFile) {
+    //   msg += ", current file = " + metadata.currentFile;
+    // }
+    // showMessage(msg);
+    // updatePercent(metadata.percent|0);
+  }).then(function (blob) {
+    console.log(blob);
+    var url = URL.createObjectURL(blob);
+    console.log(url);
+    location.href = url
+
+  })
+})
